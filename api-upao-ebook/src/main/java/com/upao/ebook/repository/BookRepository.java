9af0bab64c221155ba50cf7863e753018bba9e9a
Suclupe <jsuclupev1@upao.edu.pe>
2023-04-27 15:27:35 -0500

    Implementacion de los endpoint GET /api/v1/books para realizar el POST y GET

--- api-upao-ebook/src/main/java/com/upao/ebook/repository/BookRepository.java
+++ api-upao-ebook/src/main/java/com/upao/ebook/repository/BookRepository.java
@@ -1,4 +1,10 @@
 package com.upao.ebook.repository;
 
-public interface BookRepository {
+import com.upao.ebook.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Long> {
+
 }
--- api-upao-ebook/src/main/java/com/upao/ebook/service/BookServiceImpl.java
+++ api-upao-ebook/src/main/java/com/upao/ebook/service/BookServiceImpl.java
@@ -1,20 +1,29 @@
 package com.upao.ebook.service;
 
 import com.upao.ebook.domain.Book;
+import com.upao.ebook.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class BookServiceImpl implements BookService {
+public class BookServiceImpl implements BookService{
+
+    private final BookRepository bookRepository;
+
+    public BookServiceImpl(BookRepository bookRepository)
+    {
+        this.bookRepository = bookRepository;
+    }
 
     @Override
-    public Book createBook(Book book) {
-        return null;
+    public Book createBook(Book book){
+        return bookRepository.save(book);
     }
 
     @Override
     public List<Book> getAllBooks() {
-        return null;
+        return bookRepository.findAll();
     }
 }
+
